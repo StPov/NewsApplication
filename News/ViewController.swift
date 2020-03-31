8a6dc90225836bc5dcd1ccc93e85b91615defb4d
Stanislav <staspovolotskiy@gmail.com>
2020-03-31 09:51:58 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Stanislav Povolotskiy on 31.03.2020.
+//  Copyright © 2020 Stanislav Povolotskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
