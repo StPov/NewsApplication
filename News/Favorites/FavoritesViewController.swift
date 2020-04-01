34509a5e01ed7b23caed9bb12a790b44b121a9a0
Stanislav <staspovolotskiy@gmail.com>
2020-04-01 11:39:09 +0300

    Adding TabBra

---
+++ News/Favorites/FavoritesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesViewController.swift
+//  News
+//
+//  Created by Stanislav Povolotskiy on 01.04.2020.
+//  Copyright © 2020 Stanislav Povolotskiy. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+}
---
+++ News/News/NewsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsViewController.swift
+//  News
+//
+//  Created by Stanislav Povolotskiy on 01.04.2020.
+//  Copyright © 2020 Stanislav Povolotskiy. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+}
